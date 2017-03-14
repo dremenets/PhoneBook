4f66a4d86cf1b52f051c148384305c8609f1db83
Дмитрий Ременец <dmremenec@gmail.com>
2017-03-14 22:21:40 +0300

    Added smart-table on page

---
+++ PhoneBook/static/phoneBookController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'phoneBookController';
+
+    angular.module("PhoneBookApp").controller(controllerId, ['$scope', 'phoneBookFactory', phoneBookController])
+
+    function phoneBookController($scope, factory) {
+        $scope.contacts = [];
+
+        factory.getContacts().then(function success(data) {
+            $scope.contacts = data.data;
+        }, function error(e) {
+            console.log(e);
+        });
+    }
+})();
---
+++ PhoneBook/static/phoneBookFactory.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+    var serviceId = 'phoneBookFactory';
+
+    angular.module("PhoneBookApp").factory(serviceId, ['$http', phoneBookFactory]);
+
+    function phoneBookFactory($http) {
+
+        function getContacts() {
+            return $http.get('/api/contacts');
+        }
+
+        var service = {
+            getContacts: getContacts
+        };
+
+        return service;
+    }
+})();
