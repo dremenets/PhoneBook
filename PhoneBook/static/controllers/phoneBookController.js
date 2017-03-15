7c3a39dad2c0a1680e0f64b2fc8ece5960306568
Дмитрий Ременец <dmremenec@gmail.com>
2017-03-15 23:01:45 +0300

    Added ngDialog module

---
+++ PhoneBook/static/controllers/phoneBookController.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'phoneBookController';
+
+    angular.module("PhoneBookApp").controller(controllerId, ['$scope', 'phoneBookFactory', 'ngDialog', phoneBookController]);
+
+    function phoneBookController($scope, factory, ngDialog) {
+        $scope.contacts = [];
+
+        var loadContacts = function () {
+            factory.getContacts().then(function success(data) {
+                $scope.contacts = data.data;
+            }, function error(e) {
+                console.log(e);
+            });
+        }
+
+        loadContacts();
+
+        $scope.uploadFile = function uploadFile(files) {
+            var fd = new FormData();
+            fd.append("file", files[0]);
+            factory.upload(fd).then(function success() {
+                loadContacts();
+            }, function error(e) {
+                console.log(e);
+            });
+        };
+
+        $scope.exportFile = function exportFile() {
+            debugger;
+        };
+
+        $scope.edit = function edit(item) {
+            var itemToEdit = item;
+            ngDialog.open({
+                template: 'static/views/edit.html',
+                className: 'ngdialog-theme-default',
+                resolve: { contact: angular.copy(itemToEdit) },
+                controller: ['$scope', function ctrl ($scope, contact) {
+                    debugger
+                    $scope.contact = contact;
+
+                    $scope.save = function () {
+                        dialog.close($scope.contact);
+                    };
+
+                    $scope.close = function () {
+                        dialog.close(undefined);
+                    };
+                }]
+            });
+        };
+    }
+})();
