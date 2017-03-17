f0c054e5710aec728c6ac134f1ed85f42e3a799e
Дмитрий Ременец <dmremenec@gmail.com>
2017-03-17 20:05:13 +0300

    editForm style

--- PhoneBook/static/controllers/phoneBookController.js
+++ PhoneBook/static/controllers/phoneBookController.js
@@ -3,65 +3,67 @@
 
     var controllerId = 'phoneBookController';
 
-    angular.module("PhoneBookApp").controller(controllerId, ['$scope', 'phoneBookFactory', '$location', phoneBookController]);
+    angular.module("PhoneBookApp").controller(controllerId, [
+        '$scope', 'phoneBookFactory', '$location',
+        function($scope, factory) {
+            $scope.contacts = [];
 
-    function phoneBookController($scope, factory) {
-        $scope.contacts = [];
+            var loadContacts = function() {
+                factory.getContacts().then(function success(data) {
+                    $scope.contacts = data.data;
+                }, function error(e) {
+                    console.log(e);
+                });
+            }
 
-        var loadContacts = function() {
-            factory.getContacts().then(function success(data) {
-                $scope.contacts = data.data;
-            }, function error(e) {
-                console.log(e);
-            });
-        }
-
-        loadContacts();
-
-        $scope.importFile = function importFile(files) {
-            var fd = new FormData();
-            fd.append("file", files[0]);
-            factory.upload(fd).then(function success() {
-                loadContacts();
-            }, function error(e) {
-                console.log(e);
-            });
-        };
-
-        $scope.exportFile = function exportFile() {
-            factory.download();
-        };
-
-        $scope.editContact = function editContact(row) {
-            factory.get(row.Id).then(function success(data) {
-                data.data.BirthDate = new Date(data.data.BirthDate);
-                $scope.currentContact = data.data;
-                $('#contactForm').slideToggle();
-            }, function error(e) {
-                console.log(e);
-            });
-        };
-
-        $scope.disable = function disable() {
-            $('#contactForm').slideUp();
-        };
-
-        $scope.save = function save(item) {
-            factory.put(item).then(function success(data) {
-                $('#contactForm').slideUp();
-                loadContacts();
-            }, function error(e) {
-                console.log(e);
-            });
-        };
-
-        $scope.deleteContact = function deleteContact(item) {
-            factory.deleteContact(item.Id).then(function success(data) {
-                loadContacts();
-            }, function error(e) {
-                console.log(e);
-            });
             loadContacts();
-        };
-    }
+
+            $scope.importFile = function importFile(files) {
+                var fd = new FormData();
+                fd.append("file", files[0]);
+                factory.upload(fd).then(function success() {
+                    loadContacts();
+                }, function error(e) {
+                    console.log(e);
+                });
+            };
+
+            $scope.exportFile = function exportFile() {
+                factory.download();
+            };
+
+            $scope.editContact = function editContact(row) {
+                factory.get(row.Id).then(function success(data) {
+                    data.data.BirthDate = new Date(data.data.BirthDate);
+                    $scope.currentContact = data.data;
+                    $('#contactForm').slideToggle();
+                }, function error(e) {
+                    console.log(e);
+                });
+            };
+
+            $scope.disable = function disable() {
+                $('#contactForm').slideUp();
+            };
+
+            $scope.save = function save(item) {
+                factory.put(item).then(function success(data) {
+                    $('#contactForm').slideUp();
+                    loadContacts();
+                }, function error(e) {
+                    console.log(e);
+                });
+            };
+
+            $scope.deleteContact = function deleteContact(item) {
+                factory.deleteContact(item.Id).then(function success(data) {
+                    loadContacts();
+                }, function error(e) {
+                    console.log(e);
+                });
+                loadContacts();
+            };
+
+        }
+    ]);
 })();
--- PhoneBook/static/phoneBookApp.js
+++ PhoneBook/static/phoneBookApp.js
@@ -1,6 +1,6 @@
 ﻿
 (function () {
     'use strict';
-    var app = angular.module("PhoneBookApp", ['ui.bootstrap.datetimepicker']);
+    var app = angular.module("PhoneBookApp", []);
 
 })();
